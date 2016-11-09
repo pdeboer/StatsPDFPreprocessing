6f34fc1001d80a7a36dbd24c67d59a017110a9cb
Patrick de Boer <pdeboer@mit.edu>
2016-11-09 09:52:04 +0100

    recursive loading

--- src/main/scala/ch/uzh/ifi/pdeboer/pdfpreprocessing/pdf/PDFLoader.scala
+++ src/main/scala/ch/uzh/ifi/pdeboer/pdfpreprocessing/pdf/PDFLoader.scala
@@ -6,27 +6,29 @@ import ch.uzh.ifi.pdeboer.pdfpreprocessing.entities.{Paper, Journal}
 import ch.uzh.ifi.pdeboer.pdfpreprocessing.util.FileUtils
 
 /**
- * Created by pdeboer on 16/10/15.
- */
+  * Created by pdeboer on 16/10/15.
+  */
 class PDFLoader(path: File) {
-	def papers: Array[Paper] = {
-		path.listFiles().par.flatMap(journalDir => {
-			val resultForJournalFolder = Option(journalDir.listFiles()).getOrElse(Array.empty[File]).par.map(paperFile => {
-				extractPaper(journalFromFolder(journalDir), paperFile)
-			}).filter(_.isDefined)
+  def papers: Array[Paper] = {
+    path.listFiles().par.flatMap(journalDir => {
+      getPDFFiles(journalDir).map(f => extractPaper(journalFromFolder(journalDir), f))
+    }).filter(_.isDefined).map(_.get).toArray
+  }
 
-			if (resultForJournalFolder.nonEmpty) resultForJournalFolder else List(extractPaper(journalFromFolder(path), journalDir))
-		}).filter(_.isDefined).map(_.get).toArray
-	}
+  def getPDFFiles(folder: File): List[File] = {
+    val descendantFiles = folder.listFiles().filter(f => f.isDirectory).flatMap(f => getPDFFiles(f)).toList
+    val myPDFs = folder.listFiles().filter(f => f.getName.endsWith(".pdf")).toList
+    myPDFs ::: descendantFiles
+  }
 
-	private def journalFromFolder(journalDir: File): Journal = {
-		val journal = Journal(journalDir.getName, journalDir)
-		journal
-	}
+  private def journalFromFolder(journalDir: File): Journal = {
+    val journal = Journal(journalDir.getName, journalDir)
+    journal
+  }
 
-	private def extractPaper(journal: Journal, paperFile: File): Option[Paper] = {
-		if (paperFile.getName.endsWith(".pdf"))
-			Some(new Paper(FileUtils.filenameWithoutExtension(paperFile), paperFile, journal))
-		else None
-	}
+  private def extractPaper(journal: Journal, paperFile: File): Option[Paper] = {
+    if (paperFile.getName.endsWith(".pdf"))
+      Some(new Paper(FileUtils.filenameWithoutExtension(paperFile), paperFile, journal))
+    else None
+  }
 }
